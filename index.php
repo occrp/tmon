ea1337a24062e94c9fd8075a3521c1d1ea5bf96a
Administrator <musteatandrei@gmail.com>
2015-07-07 10:27:18 +0300

    added index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 7/7/15
+ * Time: 10:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+print_r("test.config");
+
+?>
